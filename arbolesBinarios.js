9f69dc99b6463ff829041e5b5cba9b0aaf7087f3
Alfredo Velazquez <micaminodeprogramador@gmail.com>
2022-06-15 14:40:34 -0500

    ordenamiento, pre, in y post ORDER y por niveles

--- arbolesBinarios.js
+++ arbolesBinarios.js
@@ -14,6 +14,42 @@ Tree.prototype.add = function (value) {
     }
 }
 
+Tree.prototype.preorder = function (contenedor = []) {
+    contenedor.push(this.data);
+    if (this.left !== null) this.left.preorder(contenedor);
+    if (this.right !== null) this.right.preorder(contenedor);
+
+    return contenedor;
+}
+
+Tree.prototype.inorder = function (contenedor = []) {
+    if (this.left !== null) this.left.inorder(contenedor);
+    contenedor.push(this.data);
+    if (this.right !== null) this.right.inorder(contenedor);
+
+    return contenedor;
+}
+
+Tree.prototype.postorder = function (contenedor = []) {
+    if (this.left !== null) this.left.postorder(contenedor);
+    if (this.right !== null) this.right.postorder(contenedor);
+    contenedor.push(this.data)
+
+    return contenedor
+}
+
+Tree.prototype.porNiveles = function (contenedor = [], contTree = []) {
+    contenedor.push(this.data);
+    if (this.left !== null) contTree.push(this.left);
+    if (this.right !== null) contTree.push(this.right);
+
+    if (contTree.length !== 0) {
+        contTree.shift().porNiveles(contenedor, contTree)
+    }
+
+    return contenedor;
+}
+
 let tree = new Tree(50)
 tree.add(20);
 tree.add(70);
@@ -23,6 +59,9 @@ tree.add(18);
 tree.add(82);
 tree.add(45);
 
-console.log(tree.left.right.right)
+console.log(tree.preorder())
+console.log(tree.inorder())
+console.log(tree.postorder())
+console.log(tree.porNiveles())
 
 
